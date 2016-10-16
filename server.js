87616329c2d456c2a45c67e6208b7be248fe259b
karthikeyannatesan <karthi.ltech@gmail.com>
2016-10-16 16:13:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -267,6 +267,7 @@ app.get('/', function (req, res) {
   res.send(createProfileAppTemplate(objProfileData[profileSectionName]));
 });
 
+/*
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -289,6 +290,7 @@ app.get('/article-two', function (req, res) {
 app.get('/article-three', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
+*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
