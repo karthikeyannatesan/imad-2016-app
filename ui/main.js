92d630dff574be65cfe511c80a08d0beae6a2e74
karthikeyannatesan <karthi.ltech@gmail.com>
2016-10-17 11:37:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ console.log('Loaded!');
 var imgkpnkpp = document.getElementById('kpnk-pp');
 
 imgkpnkpp.onClick = function () {
-    imgkpnkpp.style.marginLeft = '50px';
+    imgkpnkpp.style.marginLeft = '150px';
 }
